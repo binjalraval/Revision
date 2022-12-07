48e665509ed6986350ab4f60d2a100520bedf6de
binjaraval <bvs.raval30@gmail.com>
2022-12-07 20:59:18 +0000

    revision1

---
+++ src/revision1/STLC.java
@@ -0,0 +1,16 @@
+package revision1;
+
+public class STLC {public static void main(String[] args) {
+    System.out.println("Requirement Gathering");
+    System.out.println("Analysis");
+    System.out.println("Test Plan");
+    System.out.println("Test Design");
+    System.out.println("Test Execution");
+    System.out.println("Defect Reporting");
+    System.out.println("Test Closure");
+    System.out.println("Release");
+    System.out.println("Maintenance");}
+
+}
+
+}
